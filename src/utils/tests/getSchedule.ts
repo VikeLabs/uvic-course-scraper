35313c50cb843b27d9b900b5635441e0b8d5ee62
Aomi Jokoji <aomi@uvic.ca>
2020-11-02 19:21:45 -0800

    feat: add helper to grab files / paths to static assets

---
+++ src/utils/tests/getSchedule.ts
@@ -0,0 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+import appRoot from 'app-root-path';
+import globby from 'globby';
+
+const getFilePath = (term: string, subject: string, code: string) => {
+  return path.join(appRoot.toString(), `static/schedule/${term}/${subject}/${subject}_${code}.html`);
+};
+
+export const getScheduleBySubject = (term: string, subject: string) => {
+  const pattern = path.join(appRoot.toString(), `static/schedule/${term}/${subject}/*.html`);
+  return globby.sync(pattern);
+};
+
+export const getSchedule = async (term: string, subject: string, code: string) => {
+  return fs.promises.readFile(getFilePath(term, subject, code));
+};
