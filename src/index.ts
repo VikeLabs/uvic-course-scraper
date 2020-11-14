4bc5f17c5d60811bce31b2b4693e3bfe201c4532
Keith Radford <khr8771@gmail.com>
2020-11-13 16:22:44 -0800

    Create Course object for package

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,61 @@
 import cheerio from 'cheerio';
+import { getCurrTerm } from "./utils";
 import { classScheduleListingExtractor } from './pages/courseListingEntries/index';
-import { getSchedule, getScheduleBySubject, getScheduleByTerm } from './utils/tests/getSchedule';
+import { getSchedule } from './utils/tests/getSchedule';
 
-// Returns a list of CRNs given a course and it's term
-// Just a test function for the NPM package
-const getCRN = async (subject: string, course: string, term: string) =>{
-    const f =  await getSchedule(term, subject, course);
-    const $ = cheerio.load(f);
-    const parsed = await classScheduleListingExtractor($);
-    let toRet:  string[] = [];
-    parsed.forEach((element: { crn: string; }) => {
-        toRet.push(element.crn);
-    });
+const course = (subject: string, course: string, term: string = getCurrTerm()) => {
+    return new Course(subject, course, term);
+}
+class Course {
+    subject: string;
+    course: string;
+    term: string;
 
-    return toRet;
+    constructor(subject: string, course: string, term: string) {
+        this.subject = subject;
+        this.course = course;
+        this.term = term;
+    }
+
+    async getCRN() {
+        const f =  await getSchedule(this.term, this.subject, this.course);
+        const $ = cheerio.load(f);
+        const parsed = await classScheduleListingExtractor($);
+        let toRet:  string[] = [];
+        parsed.forEach((element: { crn: string; }) => {
+            toRet.push(element.crn);
+        });
+
+        return toRet;
+    }
+
+    async getSections() {
+        const f =  await getSchedule(this.term, this.subject, this.course);
+        const $ = cheerio.load(f);
+        const parsed = await classScheduleListingExtractor($);
+        let toRet:  string[] = [];
+        parsed.forEach((element: { sectionCode: string; }) => {
+            toRet.push(element.sectionCode);
+        });
+
+        return toRet;
+    }
 }
 
-export default getCRN;
+class Section extends Course {
+    section: string;
+
+    constructor(subject: string, course: string, term: string, section: string){
+        super(subject, course, term);
+        this.section = section;
+    }
+}
+
+const myClass = course('CSC', '226', '202101');
+const main = async () => {
+    console.log(await myClass.getSections());
+}
+
+main();
+
+export default course;
