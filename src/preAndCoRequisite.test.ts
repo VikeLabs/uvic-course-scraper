c6c51d9b9af51ec74a98cfe431b23fa01c4846ab
manav <shergillmanav@uvic.ca>
2020-11-16 21:40:54 -0800

    Around 60-75% cases will probably pass

--- src/preAndCoRequisite.test.ts
+++ src/preAndCoRequisite.test.ts
@@ -4,31 +4,40 @@ import { getPreAndCoReqData } from './preAndCoRequisite';
 // https://www.uvic.ca/BAN1P/bwckctlg.p_disp_cat_term_date
 
 describe('preAndCoReq information from Courses.json', (): void => {
-  const data = getPreAndCoReqData('AHVS420');
+  const data = getPreAndCoReqData('AHVS430');
 
-  const expectedCourses = [
-    'AHVS321 - Late Antique and Early Christian Art (1.5)',
-    'AHVS323 - Byzantine Art (1.5)',
-    'AHVS326 - Early Medieval Art (1.5)',
-    'AHVS328 - Gothic Art and Architecture (1.5)',
-    'HA321 - Late Antique and Early Christian Art (1.5)',
-    'HA323 - Byzantine Art (1.5)',
-    'HA326 - Early Medieval Art (1.5)',
-    'HA328 - Gothic Art and Architecture (1.5)',
-    'HIST236 - Medieval Europe (3.0)',
-    'HSTR236A - The Creation of the Medieval World (1.5)',
-  ];
+  const expectedCourses =
+    'Complete 1 of: ' +
+    'AHVS230 - Monuments of South and Southeast Asia (1.5)' +
+    'AHVS330A - Early Arts of South Asia (1.5)' +
+    'AHVS330B - Later Arts of South Asia (1.5)' +
+    'AHVS333A - Early Arts of Southeast Asia (1.5)' +
+    'AHVS333B - Later Arts of Southeast Asia (1.5)' +
+    'AHVS337 - Special Topics in Contemporary Asian Art (1.5)' +
+    'HA230 - Monuments of South and Southeast Asia (1.5)' +
+    'HA330A - Early Arts of South Asia (1.5)' +
+    'HA330B - Later Arts of South Asia (1.5)' +
+    'HA333A - Early Arts of Southeast Asia (1.5)' +
+    'HA333B - Later Arts of Southeast Asia (1.5)' +
+    'HA337 - Special Topics in Contemporary Asian Art (1.5)';
 
   it('has page has the expected courses', (): void => {
-    expect(data.all.one.courses).toEqual(expect.arrayContaining(expectedCourses));
+    expect(data.completeAllOfTheFollowing.oneCourse[0]).toEqual(expect.stringContaining(expectedCourses));
   });
 
-  const expectedUnits = ['1.5 units of 300 or 400 level MEDI courses'];
-  it('has page has the expected courses', (): void => {
-    expect(data.all.one.units).toEqual(expect.arrayContaining(expectedUnits));
+  it('has the expected standing', (): void => {
+    expect(data.completeAllOfTheFollowing.standing[0]).toBe('minimum fourth-year standing.');
+  });
+  //Next few test cases should be empty because the course tested requires "Complete all of the following"
+  const expectedEmptyArray = [''];
+  it('the expected other', (): void => {
+    expect(data.completeAllOfTheFollowing.other).toEqual(expect.arrayContaining(expectedEmptyArray));
   });
 
-  it('has page has the expected standing', (): void => {
-    expect(data.all.standing[0]).toBe('minimum fourth-year standing.');
+  it('has the expected value for CompleteOne key', (): void => {
+    expect(data.completeOne.other).toEqual(expect.arrayContaining(expectedEmptyArray));
+    expect(data.completeOne.allCourses).toEqual(expect.arrayContaining(expectedEmptyArray));
+    expect(data.completeOne.oneCourse).toEqual(expect.arrayContaining(expectedEmptyArray));
+    expect(data.completeOne.standing).toEqual(expect.arrayContaining(expectedEmptyArray));
   });
 });
--- src/preAndCoRequisite.ts
+++ src/preAndCoRequisite.ts
@@ -15,32 +15,85 @@ export function getPreAndCoReqInfo(c: string) {
   throw new Error(`No Course with the name "${c}" found`);
 }
 const preAndCoReq = {
-  all: {
-    one: { courses: [''], units: [''] },
+  completeAllOfTheFollowing: {
+    oneCourse: [''],
+    allCourses: [''],
     standing: [''],
+    other: [''],
   },
+  completeOne: { oneCourse: [''], allCourses: [''], standing: [''], other: [''] },
 };
+const data = [''];
 //Specify the course name below to get the information from courses.json
 export function getPreAndCoReqData(c: string) {
   const $ = cheerio.load(getPreAndCoReqInfo(c));
+  let i = 0;
+  if (
+    $('li > span')
+      .first()
+      .text() != 'Complete all of the following' &&
+    $('li > span')
+      .first()
+      .text() != 'Complete 1 of the following'
+  ) {
+    i = i;
+  } else {
+    data[i] = $('li > span')
+      .first()
+      .text();
+    i++;
+  }
+  $('li > [data-test]').each((index, elem) => {
+    data[i] = $(elem).text();
+    i++;
+  });
+  const regexCompleteFollowing = /Complete all of the following/;
+  const regexCompleteOneFollowing = /Complete 1 of the following/;
+  const regexCompleteAllOf = /Complete all of/;
+  const regexCompleteOneOf = /Complete 1 of/;
+  const regexStanding = /standing/;
 
-  $('[target="_blank"]').each((index, elem) => {
-    preAndCoReq.all.one.courses[index] = $(elem)
-      .parent()
-      .text();
-  });
-  $('[data-test="ruleView-B-result"]').each((index, elem) => {
-    preAndCoReq.all.standing[index] = $(elem)
-      .children()
-      .first()
-      .text();
-  });
-  $('[data-test="ruleView-A.2-result"]').each((index, elem) => {
-    preAndCoReq.all.one.units[index] = $(elem)
-      .children()
-      .first()
-      .text();
-  });
+  if (regexCompleteFollowing.test(data[0])) {
+    let x = 0;
+    let y = 0;
+    let z = 0;
+    let w = 0;
+    for (let i = 1; i < data.length; i++) {
+      if (regexCompleteAllOf.test(data[i])) {
+        preAndCoReq.completeAllOfTheFollowing.allCourses[x] = data[i];
+        x++;
+      } else if (regexCompleteOneOf.test(data[i])) {
+        preAndCoReq.completeAllOfTheFollowing.oneCourse[y] = data[i];
+        y++;
+      } else if (regexStanding.test(data[i])) {
+        preAndCoReq.completeAllOfTheFollowing.standing[z] = data[i];
+        z++;
+      } else {
+        preAndCoReq.completeAllOfTheFollowing.other[w] = data[i];
+        w++;
+      }
+    }
+  } else if (regexCompleteOneFollowing.test(data[0])) {
+    let x = 0;
+    let y = 0;
+    let z = 0;
+    let w = 0;
+    for (let i = 1; i < data.length; i++) {
+      if (regexCompleteAllOf.test(data[i])) {
+        preAndCoReq.completeOne.allCourses[x] = data[i];
+        x++;
+      } else if (regexCompleteOneOf.test(data[i])) {
+        preAndCoReq.completeOne.oneCourse[y] = data[i];
+        y++;
+      } else if (regexStanding.test(data[i])) {
+        preAndCoReq.completeOne.standing[z] = data[i];
+        z++;
+      } else {
+        preAndCoReq.completeOne.other[w] = data[i];
+        w++;
+      }
+    }
+  }
 
   return preAndCoReq;
 }
