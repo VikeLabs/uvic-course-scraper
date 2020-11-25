2de5ffc86139665d2e73fd564149263b2b31996f
Keith Radford <khr8771@gmail.com>
2020-11-25 15:10:24 -0800

    Added base for package

--- src/__tests__/index.test.ts
+++
@@ -1,9 +0,0 @@
-import classSchedules from '../index';
-
-describe('CSC226 Class Schedule', () => {
-  it('parses CSC226 correctly', async () => {
-    const myClass = await classSchedules('CSC', '226');
-    expect(myClass.getCRN()).toStrictEqual(['10770', '10771', '10772', '10773', '10774', '10775']);
-    expect(myClass.getSections()).toStrictEqual(['A01', 'A02', 'B01', 'B02', 'B03', 'B04']);
-  });
-});
