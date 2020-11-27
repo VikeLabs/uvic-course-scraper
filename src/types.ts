5adeca7ba61e169a01b27ce1847eff9cdc319109
Aomi Jokoji <aomi@uvic.ca>
2020-11-26 17:57:10 -0800

    feat: add kuali types

--- src/types.ts
+++ src/types.ts
@@ -60,3 +60,53 @@ export interface Course {
   pid: string;
   offerings: Term[];
 }
+
+// Kuali
+
+export interface SubjectCode {
+  name: string;
+  description: string;
+  id: string;
+  linkedGroup: string;
+}
+
+export interface KualiCourseItem {
+  __passedCatalogQuery: boolean;
+  description: string;
+  pid: string;
+  title: string;
+  supplementalNotes: string;
+  __catalogCourseId: string;
+  proForma: string;
+  dateStart: string;
+  credits: {
+    credits: {
+      min: string;
+      max: string;
+    };
+    value: string;
+    chosen: string;
+  };
+  crossListedCourses: {
+    __catalogCourseId: string;
+    pid: string;
+    title: string;
+  }[];
+  id: string;
+  subjectCode: SubjectCode;
+  catalogActivationDate: string;
+  hoursCatalogText: string;
+  _score: number;
+}
+
+export interface KualiCourseCatalog {
+  __catalogCourseId: string;
+  __passedCatalogQuery: boolean;
+  dateStart: string;
+  pid: string;
+  id: string;
+  title: string;
+  subjectCode: SubjectCode;
+  catalogActivationDate: string;
+  _score: number;
+}
