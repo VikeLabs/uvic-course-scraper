c13b69e8f1bf2260705670689fd870e65f6898f4
Aomi Jokoji <aomi@uvic.ca>
2020-12-12 15:11:41 -0800

    feat: add import eslint plugin and rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,45 @@
-module.exports =  {
-    parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
-    extends:  [
-      'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
-      'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-      'plugin:prettier/recommended',
-    ],
-    parserOptions:  {
-    ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
-    sourceType:  'module',  // Allows for the use of imports
-    ecmaFeatures:  {
-      jsx:  true,  // Allows for the parsing of JSX
+module.exports = {
+  plugins: ['import'],
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
     },
-    },
-    rules:  {
-        '@typescript-eslint/explicit-function-return-type': 'off',
-    },
-    reportUnusedDisableDirectives: true, // Want to make sure the disable directives are always being used
-    root: true, // This is the root eslint file (not a nested eslint file)
-    settings:  {
-      react:  {
-        version:  'detect',  // Tells eslint-plugin-react to automatically detect the version of React to use
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'import/no-unresolved': 'off',
+    'import/named': 'warn',
+    'import/namespace': 'warn',
+    'import/no-named-as-default': 'off',
+    'import/export': 'warn',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
       },
+    ],
+  },
+  reportUnusedDisableDirectives: true, // Want to make sure the disable directives are always being used
+  root: true, // This is the root eslint file (not a nested eslint file)
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
     },
-  };
+  },
+};
