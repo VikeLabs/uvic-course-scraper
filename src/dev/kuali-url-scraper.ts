a57ed182960166d59e7e4887338d567d595f67c9
Keith Radford <khr8771@gmail.com>
2021-02-02 22:25:40 -0800

    add function to scrape and place kuali endpoint URL in static file

---
+++ src/dev/kuali-url-scraper.ts
@@ -0,0 +1,26 @@
+import fs from 'fs';
+
+import got from 'got';
+
+import kualiUrls from '../../static/courses/kualiUrls.json';
+import { getCurrTerm } from '../utils';
+
+const COURSE_SEARCH_URL = 'https://www.uvic.ca/calendar/undergrad/index.php#/courses';
+
+const getKualiUrl = async (): Promise<string> => {
+  const { body } = await got(COURSE_SEARCH_URL);
+  const kualiIdRegex = /window.catalogId='(.*)'/i;
+  if (kualiIdRegex.test(body)) {
+    const slug = kualiIdRegex.exec(body)![1];
+    return `https://uvic.kuali.co/api/v1/catalog/courses/${slug}`;
+  }
+  return 'not found';
+};
+
+export const putNewKualiUrl = async (): Promise<void> => {
+  const newKuali = await getKualiUrl();
+  const term = getCurrTerm();
+  const newJson: any = { ...kualiUrls };
+  newJson[term] = newKuali;
+  fs.writeFileSync('static/courses/kualiUrls.json', JSON.stringify(newJson));
+};
