f80893de38914f72e3edebd93964d56d9044fd47
Keith Radford <khr8771@gmail.com>
2021-02-05 20:28:31 -0800

    fix linting error

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,10 +1,11 @@
 import dayjs from 'dayjs';
+
 import { getCurrentTerm } from '../utils';
 
 describe('getCurrentTerm', () => {
-    it('returns the current term', () => {
-        const date = dayjs('2021-01-01');
-        const term = getCurrentTerm(date);
-        expect(term).toEqual('202101');
-    })
-})
+  it('returns the current term', () => {
+    const date = dayjs('2021-01-01');
+    const term = getCurrentTerm(date);
+    expect(term).toEqual('202101');
+  });
+});
