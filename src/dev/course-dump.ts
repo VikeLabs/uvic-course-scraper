43a73499e18c62e11a1810631481b34e7dbed6b1
Aomi <aomijokoji@gmail.com>
2021-02-25 20:45:15 -0800

    fix: update course dumper to be term aware

--- src/dev/course-dump.ts
+++ src/dev/course-dump.ts
@@ -3,41 +3,40 @@ import { performance } from 'perf_hooks';
 
 import got from 'got';
 
-import { KualiCourseItem } from '../types';
+import { courseDetailUrl, coursesUrl } from '../common/urls';
+import { getCatalogForTerm, KualiCourseItem } from '../types';
 
 import { forEachHelper } from './utils';
 
-const COURSES_URL = 'https://uvic.kuali.co/api/v1/catalog/courses/5d9ccc4eab7506001ae4c225';
-const COURSE_DETAIL_URL = 'https://uvic.kuali.co/api/v1/catalog/course/5d9ccc4eab7506001ae4c225/';
+const writeCoursesToFS = (term: string, kualiCourseItem: KualiCourseItem[]) => {
+  fs.writeFileSync(`static/courses/courses-${term}.json`, JSON.stringify(kualiCourseItem));
 
-const writeCoursesToFS = (kualiCourseItem: KualiCourseItem[]) => {
-  fs.writeFileSync('static/courses/courses.json', JSON.stringify(kualiCourseItem));
-
+  const catalog = getCatalogForTerm(term);
   const coursesMetadata = JSON.stringify({
-    path: COURSES_URL,
-    courseDetailPath: COURSE_DETAIL_URL,
+    path: coursesUrl(catalog),
+    courseDetailPath: courseDetailUrl(catalog, ''),
     // TODO this may contain links that don't work. should remove them
     pids: kualiCourseItem.map((kualiCourseItem: KualiCourseItem) => kualiCourseItem.pid),
     datetime: Date.now(),
   });
 
-  fs.writeFileSync('static/courses/courses.metadata.json', coursesMetadata);
+  fs.writeFileSync(`static/courses/courses-${term}.metadata.json`, coursesMetadata);
 };
 
 /**
  * Downloads all courses. Saves this to courses.json and courses.metadata.json.
  */
-export const coursesUtil = async () => {
+export const coursesUtil = async (term: string): Promise<void> => {
+  const catalog = getCatalogForTerm(term);
   const courseMapper = async (kualiCourseItem: KualiCourseItem) => {
-    // ex: https://uvic.kuali.co/api/v1/catalog/course/5d9ccc4eab7506001ae4c225/r1xcyOamN
-    Object.assign(kualiCourseItem, await got(COURSE_DETAIL_URL + kualiCourseItem.pid).json());
+    Object.assign(kualiCourseItem, await got(courseDetailUrl(catalog, kualiCourseItem.pid)).json());
   };
 
   // Start timer
   const start = performance.now();
 
   console.log('Downloading all courses');
-  const kualiCourseItems: KualiCourseItem[] = await got(COURSES_URL).json();
+  const kualiCourseItems: KualiCourseItem[] = await got(coursesUrl(catalog)).json();
 
   console.log('Downloading details for each course');
   await forEachHelper(kualiCourseItems, courseMapper, 35);
@@ -48,5 +47,5 @@ export const coursesUtil = async () => {
   console.log(`Getting course data took ${(finish - start) / 60000} minutes`);
   console.log(`${(finish - start) / kualiCourseItems.length} ms/course`);
 
-  writeCoursesToFS(kualiCourseItems);
+  writeCoursesToFS(term, kualiCourseItems);
 };
