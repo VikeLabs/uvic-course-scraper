8d91d37180032c5f61ead9b8e60e45bb5a6433a9
Aomi <aomi@users.noreply.github.com>
2021-04-09 18:09:58 -0700

    Update major Jest version, minor got version and various dev-dependencies (#116)
    * chore: update dependencies
    
    * fix: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'prettier',
     'plugin:prettier/recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
