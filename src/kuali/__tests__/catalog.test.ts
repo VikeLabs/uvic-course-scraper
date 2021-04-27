00392e91d538b5350a5a0145e64988f0ced7b29c
Keith <khr8771@gmail.com>
2021-04-26 21:41:29 -0700

    add pre and coreq tests

--- src/kuali/__tests__/catalog.test.ts
+++ src/kuali/__tests__/catalog.test.ts
@@ -25,4 +25,102 @@ describe('KualiCatalogItemParser', () => {
       expect(response.credits.chosen).toEqual('range');
     });
   });
+
+  describe('parsePreCoReqs', () => {
+    describe('ATQP135 (no preAndCo or preOrCo)', () => {
+      it('gets parsed correctly', () => {
+        const details = getCourseDetailByPidSync('202101', 'HJsvjl1TB');
+
+        const response = KualiCourseItemParser(details);
+        expect(response.preAndCorequisites).toBeUndefined();
+        expect(response.preOrCorequisites).toBeUndefined();
+      });
+    });
+
+    describe('CHEM102 (preAndCo and no preOrCo)', () => {
+      it('gets parsed correctly', () => {
+        const details = getCourseDetailByPidSync('202101', 'r11A0D6XN');
+
+        const response = KualiCourseItemParser(details);
+        expect(response.preAndCorequisites).toStrictEqual([
+          {
+            quantity: 'all',
+            reqList: [
+              {
+                quantity: '1',
+                reqList: [
+                  'Chemistry 12',
+                  {
+                    quantity: 'all',
+                    reqList: ['CHEM091'],
+                  },
+                ],
+              },
+              {
+                quantity: '1',
+                reqList: ['CHEM101', 'CHEM150'],
+              },
+            ],
+          },
+        ]);
+        expect(response.preOrCorequisites).toBeUndefined();
+      });
+    });
+
+    describe('PHYS110 (preOrCo and no preAndCo)', () => {
+      it('gets parsed correctly', () => {
+        const details = getCourseDetailByPidSync('202101', 'r1SYRd67V');
+
+        const response = KualiCourseItemParser(details);
+        expect(response.preAndCorequisites).toBeUndefined();
+        expect(response.preOrCorequisites).toStrictEqual([
+          {
+            quantity: '1',
+            coreq: true,
+            reqList: ['MATH100', 'MATH102', 'MATH109'],
+          },
+        ]);
+      });
+    });
+
+    describe('CSC360 (preOrCo and preAndCo)', () => {
+      it('gets parsed correctly', () => {
+        const details = getCourseDetailByPidSync('202101', 'SkWiJ_6mE');
+
+        const response = KualiCourseItemParser(details);
+        expect(response.preAndCorequisites).toStrictEqual([
+          {
+            quantity: 'all',
+            reqList: [
+              {
+                quantity: 'all',
+                reqList: ['SENG265'],
+              },
+              {
+                quantity: '1',
+                reqList: ['CSC230', 'CENG255', 'ECE255'],
+              },
+            ],
+          },
+        ]);
+        expect(response.preOrCorequisites).toStrictEqual([
+          {
+            quantity: 'all',
+            coreq: true,
+            reqList: ['CSC226'],
+          },
+        ]);
+      });
+    });
+
+    describe('HDCC490 (preOrCo and preAndCo)', () => {
+      it('gets parsed correctly', () => {
+        const details = getCourseDetailByPidSync('202101', 'r1KjwO6QV');
+
+        const response = KualiCourseItemParser(details);
+        expect(response.preAndCorequisites).toStrictEqual(['Permission of the program.']);
+        expect(response.preOrCorequisites).toBeUndefined();
+      });
+    });
+  });
 });
