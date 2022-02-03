043f8688c09075560581c61ed08b5cf575004062
Aomi <aomijokoji@gmail.com>
2022-02-03 13:36:14 -0800

    feat: add graduate / undergrad specific catalog ids

--- src/common/__tests__/utils.test.ts
+++ src/common/__tests__/utils.test.ts
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs';
 
-import { getCatalogIdForTerm, getCurrentTerm } from '../utils';
+import { getCatalogIdByTerm, getCatalogIdForTerm, getCurrentTerm } from '../utils';
 
 describe('getCurrentTerm', () => {
   it('returns the current term', () => {
@@ -10,9 +10,49 @@ describe('getCurrentTerm', () => {
   });
 });
 
+describe('getCatalogIdByTerm', () => {
+  describe('undergraduate', () => {
+    const level = 'undergraduate';
+    it('returns the catalog ID for Fall 2020', () => {
+      expect(getCatalogIdByTerm('202009', level)).toBe('5eb09dad7a30f7001af74eb1');
+    });
+
+    it('returns the catalog ID for Winter 2021', () => {
+      expect(getCatalogIdByTerm('202101', level)).toBe('5f21b66d95f09c001ac436a0');
+    });
+
+    it('returns the catalog ID for Summer 2021', () => {
+      expect(getCatalogIdByTerm('202105', level)).toBe('5ff357f8d30280001b0c26dd');
+    });
+
+    it('returns an empty string for a invalid term', () => {
+      expect(getCatalogIdByTerm('202000', level)).toBeNull();
+    });
+  });
+
+  describe('graduate', () => {
+        const level = 'graduate';
+        it('returns the catalog ID for Fall 2020', () => {
+      expect(getCatalogIdByTerm('202009', level)).toBe('5eb09c845f06e0001a5ad4ea');
+    });
+
+    it('returns the catalog ID for Winter 2021', () => {
+      expect(getCatalogIdByTerm('202101', level)).toBe('5f21b607847fd9001a54a406');
+    });
+
+    it('returns the catalog ID for Summer 2021', () => {
+      expect(getCatalogIdByTerm('202105', level)).toBe('5ff377d15e3876001b6dbd0d');
+    });
+
+    it('returns an empty string for a invalid term', () => {
+      expect(getCatalogIdByTerm('202000', level)).toBeNull();
+    });
+  })
+});
+
 describe('getCatalogIdForTerm', () => {
   it('returns the catalog ID for Fall 2020', () => {
-    expect(getCatalogIdForTerm('202009')).toBe('5d9ccc4eab7506001ae4c225');
+    expect(getCatalogIdForTerm('202009')).toBe('5eb09dad7a30f7001af74eb1');
   });
 
   it('returns the catalog ID for Winter 2021', () => {
--- src/common/utils.ts
+++ src/common/utils.ts
@@ -20,9 +20,7 @@ function monthToTerm(month: number): term {
   }
 }
 
-function mod(n: number, m: number): number {
-  return ((n % m) + m) % m;
-}
+const mod = (n: number, m: number) => ((n % m) + m) % m;
 
 function getTermString(currentYear: number, currentTerm: term, delta: number): string {
   const termNumber = currentTerm.valueOf() + delta;
@@ -67,10 +65,23 @@ export function getCurrentTerm(date: dayjs.Dayjs = dayjs()): string {
   return year + month;
 }
 
-export function getCatalogIdForTerm(term: string): string {
+export function getCatalogIdByTerm(term: string, level: 'undergraduate' | 'graduate'): string | null {
+  switch (level) {
+    case 'undergraduate':
+      return getUndergradCatalogIdByTerm(term);
+    case 'graduate':
+      return getGradCatalogIdByTerm(term);
+    default:
+      return null;
+  }
+}
+
+function getUndergradCatalogIdByTerm(term: string): string | null {
   switch (term) {
-    case '202009':
+    case '202005':
       return '5d9ccc4eab7506001ae4c225';
+    case '202009':
+      return '5eb09dad7a30f7001af74eb1';
     case '202101':
       return '5f21b66d95f09c001ac436a0';
     case '202105':
@@ -78,9 +89,37 @@ export function getCatalogIdForTerm(term: string): string {
     case '202109':
       return '6092db6ebae543001bc23ca1';
     case '202201':
-      // TODO: cannot find a separate calendar for Winter 2022
-      return '6092db6ebae543001bc23ca1';
+      return '61325cd1a7d5ec3519bf62a0';
     default:
-      return '';
+      return null;
   }
+}
+
+function getGradCatalogIdByTerm(term: string): string | null {
+  switch (term) {
+    case '202005':
+      return '5db9a19063f365001a66a451';
+    case '202009':
+      return '5eb09c845f06e0001a5ad4ea';
+    case '202101':
+      return '5f21b607847fd9001a54a406';
+    case '202105':
+      return '5ff377d15e3876001b6dbd0d';
+    case '202109':
+      return '6092db40bae543001bc23ca0';
+    case '202201':
+      return '61325d5b705e91473edf95ea';
+    default:
+      return null;
+  }
+}
+
+/**
+ *
+ * @param term
+ * @returns
+ * @deprecated use getCatalogIdByTerm instead
+ */
+export function getCatalogIdForTerm(term: string): string {
+  return getUndergradCatalogIdByTerm(term) ?? '';
 }
